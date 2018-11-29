0443e2586ca8b6f24954978a75683fe010e0a2ee
Luisa Pätzold <luisa.paetzold@arcor.de>
2018-11-29 11:57:12 +0100

    Added item healing functionality + tests

--- Test Driven Game Development/Assets/Scripting/Scripts/Item.cs
+++ Test Driven Game Development/Assets/Scripting/Scripts/Item.cs
@@ -5,6 +5,61 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "TTGD/Item")]
 public class Item : ScriptableObject
 {
+    public ItemType type;
+    public int MaxUses = 3;
+    private int usesLeft;
+
+    public Item()
+    {
+        SetUpItem();
+    }
+
+    public void SetUpItem()
+    {
+        usesLeft = MaxUses;
+    }
+
+    public void Use(FighterStatsClass user) 
+    {
+        if (usesLeft == 0)
+        {
+            Debug.LogWarning("Tried to use item when it had 0 uses left. No effect!");
+            return;
+        }
+        if (user == null)
+        {
+            Debug.LogWarning("Tried to use item without a user. No effect!");
+            return;
+        }
+
+        switch (type)
+        {
+            case ItemType.AttackBoost:
+                break;
+            case ItemType.Healing:
+                user.GetHealedBy(20);
+                break;
+            default:
+                Debug.LogWarning("There was no behavior specified for item of type " + type.ToString());
+                break;
+        }
+        usesLeft--;
+    }
 
 
+    public int GetMaxUses()
+    {
+        return MaxUses;
+    }
+
+    public int GetUsesLeft()
+    {
+        return usesLeft;
+    }
+}
+
+public enum ItemType
+{
+    Healing,
+    AttackBoost
 }
---
+++ Test Driven Game Development/Assets/Testing/Test_Item.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class Test_Item {
+
+    [Test]
+    public void Test_ItemCannotBeUsedMoreThanMaxUses()
+    {
+        PlayerStatsClass stats = new PlayerStatsClass();
+        Item item = ScriptableObject.CreateInstance<Item>();
+        Assert.AreEqual(3, item.GetUsesLeft(), "Item uses did not start at the expected value!");
+
+        item.Use(stats);
+        Assert.AreEqual(2, item.GetUsesLeft(), "Item uses did not decrease as the item was used!");
+        item.Use(stats);
+        Assert.AreEqual(1, item.GetUsesLeft(), "Item uses did not decrease as the item was used!");
+        item.Use(stats);
+        Assert.AreEqual(0, item.GetUsesLeft(), "Item uses did not decrease as the item was used!");
+
+        item.Use(stats);
+
+        Assert.AreNotEqual(-1, item.GetUsesLeft(), "Item could be used even though it had 0 uses left!");
+        LogAssert.Expect(LogType.Warning, "Tried to use item when it had 0 uses left. No effect!");
+    }
+
+    [Test]
+    public void Test_ItemNeedsUserToBeUsed()
+    {
+        Item item = ScriptableObject.CreateInstance<Item>();
+        item.Use(null);
+
+        Assert.AreEqual(3, item.GetUsesLeft(), "Item uses were decreased even though no user was set!");
+        LogAssert.Expect(LogType.Warning, "Tried to use item without a user. No effect!");
+    }
+
+    [Test]
+    public void Test_ItemCanHealPlayer()
+    {
+        PlayerStatsClass stats = new PlayerStatsClass();
+        stats.ReceiveDamage(50);
+        int damagedHealth = stats.GetCurrentHealth();
+
+        Item item = ScriptableObject.CreateInstance<Item>();
+        item.type = ItemType.Healing;
+        item.Use(stats);
+
+        int healedHealth = stats.GetCurrentHealth();
+
+        Assert.Less(damagedHealth, healedHealth, "Using a healing item did not increade the players health!");
+    }
+}
