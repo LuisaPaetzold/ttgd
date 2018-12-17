bc623a8f672f2b6f93943cf8e2c0b534b1d78f18
Luisa Pätzold <luisa.paetzold@arcor.de>
2018-12-17 23:41:42 +0100

    Sky now changes time of day

---
+++ Test Driven Game Development/Assets/PlayModeTesting/Test_PMSkyController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class Test_PMSkyController
+{
+    [UnityTest]
+    public IEnumerator Test_PMSkyControllerOffsetStartsAtZero()
+    {
+        SkyController skyCtr = CreateSkyController();
+        Assert.Zero(skyCtr.skyMat.mainTextureOffset.x, "Sky material offset did not start at 0!");
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator Test_PMSkyControllerOffsetIsIncreasedByStepSize()
+    {
+        SkyController skyCtr = CreateSkyController();
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+        Assert.NotZero(skyCtr.skyMat.mainTextureOffset.x, "Sky material offset was not changed at all!");
+        Assert.AreEqual(skyCtr.skyMat.mainTextureOffset.x, skyCtr.stepSize, "Sky material offset did not increase by step size!");
+    }
+
+    [UnityTest]
+    public IEnumerator Test_PMSkyControllerOffsetIsResetToZeroWhenOverOne()
+    {
+        SkyController skyCtr = CreateSkyController();
+        skyCtr.skyMat.mainTextureOffset = new Vector2(1, 0);
+        yield return new WaitForEndOfFrame();
+        Assert.Zero(skyCtr.skyMat.mainTextureOffset.x, "Sky material was not reset to 0 after exceeding 1!");
+    }
+
+    // ------------- helper methods ------------------
+
+    public SkyController CreateSkyController()
+    {
+        SkyController s = new GameObject().AddComponent<SkyController>();
+        s.skyMat = new Material(Shader.Find("Unlit/Texture"));
+        return s;
+    }
+}
---
+++ Test Driven Game Development/Assets/Scripting/Scripts/SkyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyController : MonoBehaviour {
+
+    public Material skyMat;
+    [Range(0, 0.001f)]
+    public float stepSize = 0.0002f;
+
+    private void Start()
+    {
+        if (skyMat != null)
+        {
+            skyMat.mainTextureOffset = new Vector2(0, 0);
+        }
+    }
+
+    void Update ()
+    {
+		if (skyMat != null)
+        {
+            skyMat.mainTextureOffset += new Vector2(stepSize, 0);
+            if (skyMat.mainTextureOffset.x >= 1)
+            {
+                skyMat.mainTextureOffset = new Vector2(0, 0);
+            }
+        }
+	}
+}
